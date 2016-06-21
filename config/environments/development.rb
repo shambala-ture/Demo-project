de064ac56c78c02213da4fc4941d4f62148652ac
shambala.ture <shambala.ture@wwindia.com>
2016-06-21 12:48:44 +0530

    fix for assets compressor issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.assets.css_compressor = :yui
-  config.assets.css_compressor = :sass
-  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :yui
+  # config.assets.css_compressor = :sass
+  # config.assets.js_compressor = :uglifier
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
